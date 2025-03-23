7379368cd94809a1b661072f362f9f180684702a
LillianfR <lillianfaithreynolds@gmail.com>
2025-03-23 18:57:49 -0400

    Add files via upload

--- strings.js
+++ strings.js
@@ -25,46 +25,54 @@ function strings()
 
 }
 //login functions
-function checkCreds()
-{
-    var firstName = document.getElementById("fName").value;
-    var lastName = document.getElementById("lName").value;
-    var fullName = firstName +  " " + lastName;
-    var badgeNum = document.getElementById("badgeID").value;
-    var passW = document.getElementById("PW").value;
+function checkCreds(){
+    // Get the values entered by the user for first name, last name, badge number, and password
 
-//first name
-    if (fullName.length >= 20)
-    {
-        document.getElementById("loginStatus").innerHTML = "Names must have values";
-    }
-    else if (firstName !="Lillian")
-    {
-         document.getElementById("loginStatus").innerHTML = "Incorrect name";
-    }
-    //last name 
-    else if (lastName !="Reynolds")
-    {
-         document.getElementById("loginStatus").innerHTML = "Incorrect name";
-    }
+   var firstName = document.getElementById("fName").value;
+   var lastName = document.getElementById("lName").value;
+   var fullName = firstName + "" + lastName; 
+   // Combine first name and last into a full name string
+   var badgeNum = document.getElementById("badgeID").value;
+   var passW = document.getElementById("PW").value;
 
-    //badge num
-    else if (badgeNum !="100")
-    {
-        document.getElementById("loginStatus").innerHTML = "Invalid badge num";
-    }
-    //password
-    else if (passW !="Meow")
-     {
-         document.getElementById("loginStatus").innerHTML = "Invalid Password";
-     }
-
-     //results
-    else
-     
-     {
-        document.getElementById("loginStatus").innerHTML = "Accepted!!!";
-
-        location.replace("strings.html")
-     }
-    }
+    // Check if the combined full name length is greater than 20 characters
+   if (fullName.length > 20) 
+   {
+       document.getElementById("loginStatus").innerHTML = "Invalid Name Length"; 
+       // Display message in case of invalid full name length
+   }
+   
+   // Check if first name length is valid
+   else if (firstName.length == 7)
+   {
+       document.getElementById("loginStatus").innerHTML = "Enter First Name"; 
+       // Display message for invalid First Name length
+   }
+   
+    // Check if last name length is valid
+   else if (lastName.length == 0)
+       {
+           document.getElementById("loginStatus").innerHTML = "Enter Last Name"; // Display message for invalid Last Name length
+       }
+   
+       // Check if the badge number is NOT exactly 5 characters long
+   if (badgeNum.length !== 5)
+       {
+           document.getElementById("loginStatus").innerHTML = "Invalid Badge Number"; return;  // Display an error message if the badge number is invalid
+       }
+   
+   
+   // Check if the password entered does not match "UATRocks" (invalid if true)
+   else if (passW !="UATRocks")
+   {
+       document.getElementById("loginStatus").innerHTML = "Invalid password"; // Display message for incorrect password
+   }
+   
+   // If all checks are passed, display "Accepted!" and redirect the user to "strings.html"
+   else
+   {
+       document.getElementById("loginStatus").innerHTML = "Accepted!";
+       location.replace("strings.html") 
+       // Redirect to another page
+   }
+}
